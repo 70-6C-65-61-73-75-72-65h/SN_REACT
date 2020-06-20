01613a9cc4210f9f0006c052abb8b65c1ac36adf
Max Ulshin <max.ulshin.max@istat.com.ua>
2020-06-20 19:19:47 +0300

    Updates

---
+++ precache-manifest.333fb2209903d57eaece1f7edee70f88.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196cc60680abbc3624f1bbe2c67afdd5",
+    "url": "/SN_REACT/index.html"
+  },
+  {
+    "revision": "93ada13c939605f02a59",
+    "url": "/SN_REACT/static/css/4.2744c19f.chunk.css"
+  },
+  {
+    "revision": "bda9c18b7f3baddac734",
+    "url": "/SN_REACT/static/css/5.21a90b6f.chunk.css"
+  },
+  {
+    "revision": "c65a014ce2ee739faf27",
+    "url": "/SN_REACT/static/css/6.c9e4fe20.chunk.css"
+  },
+  {
+    "revision": "93777bc2abaf6dda71e6",
+    "url": "/SN_REACT/static/css/7.98c28887.chunk.css"
+  },
+  {
+    "revision": "f7bdc2dd63d3b53b0266",
+    "url": "/SN_REACT/static/css/main.6c853099.chunk.css"
+  },
+  {
+    "revision": "fbd2fa17e730cc26c6b3",
+    "url": "/SN_REACT/static/js/0.69982eac.chunk.js"
+  },
+  {
+    "revision": "4bd56f70f2ec568c3b9f",
+    "url": "/SN_REACT/static/js/3.a3eb4493.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SN_REACT/static/js/3.a3eb4493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ada13c939605f02a59",
+    "url": "/SN_REACT/static/js/4.570b6d4e.chunk.js"
+  },
+  {
+    "revision": "bda9c18b7f3baddac734",
+    "url": "/SN_REACT/static/js/5.02414d58.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/SN_REACT/static/js/5.02414d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65a014ce2ee739faf27",
+    "url": "/SN_REACT/static/js/6.8829d326.chunk.js"
+  },
+  {
+    "revision": "93777bc2abaf6dda71e6",
+    "url": "/SN_REACT/static/js/7.2766c34a.chunk.js"
+  },
+  {
+    "revision": "f7bdc2dd63d3b53b0266",
+    "url": "/SN_REACT/static/js/main.95570c35.chunk.js"
+  },
+  {
+    "revision": "4a8a1aef35793fd25046",
+    "url": "/SN_REACT/static/js/runtime-main.1837e005.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/SN_REACT/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/SN_REACT/static/media/user.8b1dbb1f.png"
+  }
+]);
